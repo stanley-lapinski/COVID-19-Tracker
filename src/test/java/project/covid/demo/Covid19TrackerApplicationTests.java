8af58d05c710992051579eb312d2f117391c503d
stanley-lapinski <stanis.lapinski@gmail.com>
2020-03-21 18:05:06 +0100

    first general commit

---
+++ src/test/java/project/covid/demo/Covid19TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package project.covid.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19TrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
