c86fd6af29a44f379cc3bfa201f716b0ec86d8e8
stanley-lapinski <stanis.lapinski@gmail.com>
2020-03-21 18:47:35 +0100

    added bootstrap files

--- src/main/java/project/covid/Covid19TrackerApplication.java
+++ src/main/java/project/covid/Covid19TrackerApplication.java
@@ -1,4 +1,4 @@
-package project.covid.demo;
+package project.covid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/project/covid/Covid19TrackerApplicationTests.java
+++ src/test/java/project/covid/Covid19TrackerApplicationTests.java
@@ -1,4 +1,4 @@
-package project.covid.demo;
+package project.covid;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
